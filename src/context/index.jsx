fa23416a3a9d27ed6f33d7b2fef28d08c6f59607
jdu214843 <214843a@jdu.uz>
2023-06-10 00:00:04 +0500

    Generic and Konfigures

---
+++ src/context/index.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const Root = createContext();
+
+const RootContext = ({ children }) => {
+  return <Root.Provider>{children}</Root.Provider>;
+};
+export default RootContext;
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import { Home } from "../components/Home";
 
 export const Root = () => {
-  return <div>Root</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<h1>Properties</h1>} />
+        <Route path="*" element={<h1>NOT FOUND DATA</h1>} />
+        <Route path="/" element={<Navigate to={"/home"}></Navigate>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
+export default Root;
